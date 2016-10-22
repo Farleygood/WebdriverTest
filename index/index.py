2aa7d8967ac82bdb4da3d8b54c7dcf0abd9f9f3c
Farleygood <44248556@qq.com>
2016-10-22 14:09:21 +0800

    添加index模块

--- index/index.py
+++ index/index.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
+
+from baidu.baiduHq import login
+from selenium import webdriver
+
+'''
+忽略打开浏览器时提示 ignore-certificate-errors
+'''
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.maximize_window()
+driver.implicitly_wait(15)
+driver.get('https://www.baidu.com')
+login(driver, 'xxxxx', 'xxxxx')
+driver.quit()
