1a45fb91904283e9348fd1deb0154842356d7f9f
Farleygood <44248556@qq.com>
2016-10-22 14:02:02 +0800

    添加登录百度模块

--- baidu/baiduHq.py
+++ baidu/baiduHq.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
+
+import time
+
+def wait():
+	time.sleep(3)
+
+def login(driver, username, password):
+	wait()
+	driver.find_element_by_id('u1').find_element_by_class_name('lb').click()
+	wait()
+	driver.find_element_by_id('TANGRAM__PSP_8__userName').send_keys('xxxxx')
+	wait()
+	driver.find_element_by_id('TANGRAM__PSP_8__password').send_keys('xxxxx')
+	wait()
+	driver.find_element_by_id('TANGRAM__PSP_8__submit').click()
+	wait()
+
+	assert driver.find_element_by_xpath(".//*[@id='s_username_top']/span")
+	print u'登录成功！'
