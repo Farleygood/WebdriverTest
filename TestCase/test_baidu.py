1dcbf4f04fed694b6529ab2dec894ab3fce179c1
Farleygood <44248556@qq.com>
2016-11-01 18:23:45 +0800

    Add log

--- TestCase/test_baidu.py
+++ TestCase/test_baidu.py
@@ -3,16 +3,31 @@
 __author__ = 'Administrator'
 
 from selenium import webdriver
+from Page.BaseTestPage import BaseTestCase
+from Page.HomePage import LoginHomePage
+from Page.BasePage import Page_Loc
+from Page.MessageCount import Message_Count
 import time as t
-'''
-忽略打开浏览器时提示 ignore-certificate-errors
-'''
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
-driver = webdriver.Chrome(chrome_options=options)
+import unittest
+# '''
+# 忽略打开浏览器时提示 ignore-certificate-errors
+# '''
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+# driver = webdriver.Chrome(chrome_options=options)
+# 
+# driver.maximize_window()
+# driver.implicitly_wait(15)
+# driver.get('http://www.51cto.com/')
+# t.sleep(5)
+# driver.quit()
 
-driver.maximize_window()
-driver.implicitly_wait(15)
-driver.get('http://www.51cto.com/')
-t.sleep(5)
-driver.quit()
+class MyCtoBlog(LoginHomePage,Page_Loc,BaseTestCase):
+    
+    # 测试51cto登录成功后返回消息总数
+    def test_001(self,username,password):
+        self.Login('username','password')
+        
+    
+    
+    
--- allTests.py
+++ allTests.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 __author__ = 'Administrator'
+
+import unittest,os,sys,HTMLTestRunner,time
+from Log import FinalLogger
+import logging
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# 创建日志实例
+logger = FinalLogger.getLogger()
+
+def suite():
+    dir_case = unittest.defaultTestLoader.discover('F:/Git/WebdriverTest/TestCase',\
+                            pattern = 'test_*.py', \
+                            top_level_dir = None
+                        )
+    return dir_case
+def getNowTime():
+    return time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
+    
+def runAutomation():
+    filename = 'F:/Git/WebdriverTest/Report/' + getNowTime() + 'TestReport.html'
+    
+    #打印日志级别为info
+    logger.info(u'loginfo: 文件创建成功！......')
+
+    fp = file(filename,'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(
+            stream = fp,
+            title = u'自动化测试报告',
+            description = u'自动化测试详细信息'
+        )
+    fp.close()
+    
+    # 自动化测试报告已关闭
+    logger.info(u'loginfo: 自动化测试报告已关闭！')
+    runner.run(suite())
+
+if __name__ == '__main__':
+    runAutomation()
+    
+    
