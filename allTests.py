d67951ead79d00253987f40a7cf9d83caa034934
Farleygood <44248556@qq.com>
2017-01-04 22:21:20 +0800

    修改path

--- allTests.py
+++ allTests.py
@@ -4,37 +4,38 @@
 __author__ = 'Administrator'
 
 import unittest,os,sys,HTMLTestRunner,time
-import LogConfig,logging
+import logging
+from Model import LogConfig
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def suite():
     logging.info(u'运行suite……')
-    dir_case = unittest.defaultTestLoader.discover('F:/Git/WebdriverTest/TestCase',\
-                            pattern = 'test_*.py', \
-                            top_level_dir = None
-                        )
+    dir_case = unittest.defaultTestLoader.discover(
+        'F:/GitHub/GitHib_Project/WebdriverTest/TestCase/',
+        pattern='test_*.py',
+        top_level_dir=None
+    )
     logging.info(u'suite运行完毕……')
     return dir_case
 def getNowTime():
     return time.strftime('%Y-%m-%d %H_%M_%S',time.localtime(time.time()))
     
 def runAutomation():
-    filename = 'F:/Git/WebdriverTest/Report/' + getNowTime() + 'TestReport.html'
+    filename = 'F:/GitHub/GitHib_Project/WebdriverTest/Report/' + getNowTime() + 'TestReport.html'
     
     #打印日志级别为info
     logging.info(u'loginfo: 文件创建成功！......')
 
     fp = file(filename,'wb')
     runner = HTMLTestRunner.HTMLTestRunner(
-            stream = fp,
-            title = u'自动化测试报告',
-            description = u'自动化测试详细信息'
-        )
+        stream=fp,
+        title=u'自动化测试报告',
+        description=u'自动化测试详细信息'
+    )
     runner.run(suite())
 
 if __name__ == '__main__':
     runAutomation()
-    
-    
+
