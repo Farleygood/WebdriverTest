64745671217e1761969dcbd61118ea84c568d7bd
Farleygood <44248556@qq.com>
2017-01-04 22:10:39 +0800

    modify log file path

--- Model/LogConfig.py
+++ Model/LogConfig.py
@@ -5,16 +5,16 @@ __author__ = 'Administrator'
 import logging
 import time
 # 配置日志信息
-# log_file = "F:/Git/WebdriverTest/Webdriver.log"
+# log_file = "F:/Git/WebdriverTest/logs/Webdriver.log"
 # 打印年月日，每天存储一个日志文件
 time = time.strftime('%Y%m%d',time.localtime(time.time()))
 
 logging.basicConfig(level=logging.DEBUG,
-          format='[%(levelname)s] [%(funcName)s] [%(asctime)s]%(message)s',
-          datefmt='%m-%d %H:%M',
-          # 每天存储一个日志文件,如果想覆盖log改filemode='w'
-          filename='F:/Git/WebdriverTest/Webdriver_%s.log'%time,
-          filemode='a+')
+                    format='[%(levelname)s] [%(funcName)s] [%(asctime)s]%(message)s',
+                    datefmt='%m-%d %H:%M',
+                    # 每天存储一个日志文件,如果想覆盖log改filemode='w'
+                    filename='F:/GitHub/GitHib_Project/WebdriverTest/logs/WebdriverTest_%s.log'%time,
+                    filemode='a+')
 
 # 定义一个Handler打印INFO及以上级别的日志到sys.stderr
 console = logging.StreamHandler()
