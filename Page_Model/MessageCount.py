e55ad15dd3d731052e3625d205f554ed0f4324f2
Farleygood <44248556@qq.com>
2017-01-04 22:19:26 +0800

    修改登陆51cto博客后获取未读消息数

--- Page_Model/MessageCount.py
+++ Page_Model/MessageCount.py
@@ -2,16 +2,14 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
 
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from BasePage import Page_Loc
-import LogConfig,logging
+from BasePage import WebUI
+import logging
 
-class Message_Count(Page_Loc):
-    LogConfig.logging.info(u'增加日志模块…….')
+class Message_Count(WebUI):
+    logging.info(u'增加日志模块…….')
     message_loc = (By.XPATH,'//*[@id="login_status"]/div/a[2]')
     
     def GetMessageCount(self):
         self.wait
-        return self.find_element(*self.message_loc).text
-    
+        return self.driver.find_element(*self.message_loc).text
