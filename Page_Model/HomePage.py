8556076b31aa49468a3b582afa45ea2d581df8da
Farleygood <44248556@qq.com>
2017-01-04 22:18:27 +0800

    修改登陆51cto页面的函数

--- Page_Model/HomePage.py
+++ Page_Model/HomePage.py
@@ -5,60 +5,47 @@ __author__ = 'Administrator'
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from BasePage import Page_Loc
+from BasePage import WebUI
 from MessageCount import Message_Count
 from selenium.webdriver.common.action_chains import ActionChains
 
-class LoginHomePage(Page_Loc):
-	
-	click_loc = (By.XPATH,'//*[@id="login-form"]/div[3]/input')
-	userName_loc = (By.ID,"loginform-username")
-	password_loc = (By.ID,"loginform-password")
-	clickButton_loc = (By.XPATH,'//*[@id="login-form"]/div[3]/input')
-	error_loc = (By.XPATH,'//*[@id="login-form"]/div[2]/div/div')
+class LoginHomePage(WebUI):
 
-	def Click(self):
-		self.wait
-		self.find_element(*self.click_loc).click()
-	
-	def getUserTextField(self,username):
-		self.wait
-		self.find_element(*self.userName_loc).send_keys(username)
+    click_loc = (By.XPATH,'//*[@id="login-form"]/div[3]/input')
+    userName_loc = (By.ID,"loginform-username")
+    password_loc = (By.ID,"loginform-password")
+    clickButton_loc = (By.XPATH,'//*[@id="login-form"]/div[3]/input')
+    error_loc = (By.XPATH,'//*[@id="login-form"]/div[2]/div/div')
 
-	def getPasswordField(self,password):
-		self.wait
-		self.find_element(*self.password_loc).send_keys(password)
-	
-	def getSubmitButton(self):
-		self.wait
-		self.find_element(*self.clickButton_loc).click()
+    def Click(self):
+        self.wait
+        self.findElement(*self.click_loc).click()
 
-	# 获取登录错误的提示信息	
-	def GetErrorText(self):
-		self.wait
-		self.find_element(*self.error_loc).text
+    def getUserTextField(self,username):
+        self.wait
+        self.findElement(*self.userName_loc).send_keys(username)
 
-	# 登录函数,登录成功后返回消息总数
-	def Login(self,username,password):
-		self.wait
-		self.doLogin(username, password)
-		return Message_Count(self.driver)
-		
-	def doLogin(self,username,password):
-		self.Click()
-		self.getUserTextField(username)
-		self.getPasswordField(password)
-		self.getSubmitButton()
-	
-	
-	
+    def getPasswordField(self,password):
+        self.wait
+        self.findElement(*self.password_loc).send_keys(password)
 
+    def getSubmitButton(self):
+        self.wait
+        self.findElement(*self.clickButton_loc).click()
 
+    # 获取登录错误的提示信息
+    def GetErrorText(self):
+        self.wait
+        self.findElement(*self.error_loc).text
 
-	
-	
+    # 登录函数,登录成功后返回消息总数
+    def Login(self,username,password):
+        self.wait
+        self.doLogin(username, password)
+        return Message_Count(self.driver)
 
-	
-
-	
-
+    def doLogin(self,username,password):
+        self.Click()
+        self.getUserTextField(username)
+        self.getPasswordField(password)
+        self.getSubmitButton()
