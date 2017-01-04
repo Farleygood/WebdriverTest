9c95f4f67db176cff88b860f9039a6bf8bf980aa
Farleygood <44248556@qq.com>
2017-01-04 22:16:06 +0800

    添加工厂方法

--- Page_Model/BasePage.py
+++ Page_Model/BasePage.py
@@ -2,27 +2,68 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
 
-from selenium import webdriver
-from appium import webdriver
 from selenium.webdriver.support.expected_conditions import NoSuchElementException
-from selenium.webdriver.common.by import By
 import time as t
 
+'''
 class Page_Loc(object):
-    
-    def __init__(self,driver):
+
+    def __init__(self, driver):
         self.driver = driver
     
-    def find_element(self,*loc):
+    def findelement(self,*loc):
         try:
             return self.driver.find_element(*loc)
         except(NoSuchElementException,ValueError,KeyError,Exception),e:
-            print 'Error details: %S' %(e.args(0))
+            print 'Error details: %S' %(e.args[0])
             
     @property
     def wait(self):
         t.sleep(3)
-    
-        
-        
+'''
 
+class Factory(object):
+    def __init__(self,driver):
+        self.driver=driver
+
+    #工厂方法
+    def createWebDdriver(self,webDdriver):
+        if webDdriver=='web':
+            return WebUI(self.driver)
+        elif webDdriver=='app':
+            return AppUI(self.driver)
+
+class WebDdriver(object):
+    def __init__(self,driver):
+        self.driver=driver
+
+    def __str__(self):
+        return 'webDdriver'
+
+    def findElement(self,*loc):
+        try:
+            return self.driver.find_element(*loc)
+        except NoSuchElementException,e:
+            print 'Error details :%s'%(e.args[0])
+
+    def findsElement(self,*loc):
+        try:
+            return self.driver.find_elements(*loc)
+        except NoSuchElementException,e:
+            print 'Error details :%s'%(e.args[0])
+
+    def getScreenshot(self,name,form='png'):
+        t.sleep(2)
+        self.driver.get_screenshot_as_file('D:/git/Python/webdriverHq/img/'+name+"."+form)
+
+    @property
+    def wait(self):
+        t.sleep(2)
+
+class WebUI(WebDdriver):
+    def __str__(self):
+        return 'WEB UI'
+
+class AppUI(WebDdriver):
+    def __str__(self):
+        return 'App UI'
