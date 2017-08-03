67bafded652f0f3103e17f41b61aeadb92777d5f
Farleygood <44248556@qq.com>
2017-08-03 14:43:26 +0800

    修改日志文件

--- Model/LogConfig.py
+++ Model/LogConfig.py
@@ -10,17 +10,18 @@ import time
 time = time.strftime('%Y%m%d',time.localtime(time.time()))
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='[%(levelname)s] [%(funcName)s] [%(asctime)s]%(message)s',
+                    format="[%(levelname)s] [%(module)s] [%(funcName)s] [line %(lineno)s] [%(asctime)s] %(message)s",
                     datefmt='%m-%d %H:%M',
                     # 每天存储一个日志文件,如果想覆盖log改filemode='w'
-                    filename='F:/GitHub/GitHib_Project/WebdriverTest/logs/WebdriverTest_%s.log'%time,
+                    # filename='F:/GitHub/GitHib_Project/WebdriverTest/logs/WebdriverTest_%s.log'%time,
+                    filename='E:/Git/WebdriverTest/logs/WebdriverTest_%s.log'%time,
                     filemode='a+')
 
 # 定义一个Handler打印INFO及以上级别的日志到sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 # 设置日志打印格式
-formatter = logging.Formatter('[%(levelname)s] [%(funcName)s] [%(asctime)s]%(message)s')
+formatter = logging.Formatter("[%(levelname)s] [%(module)s] [%(funcName)s] [line %(lineno)s] [%(asctime)s] %(message)s")
 
 console.setFormatter(formatter)
 # 将定义好的console日志handler添加到root logger
