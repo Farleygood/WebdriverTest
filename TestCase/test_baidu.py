3a39cf7bce87508c95edaff5993d8584a5b9162f
Farleygood <44248556@qq.com>
2017-08-03 14:46:04 +0800

    分离出web脚本

--- TestCase/test_baidu.py
+++ TestCase/test_baidu.py
@@ -2,21 +2,21 @@
 # -*- coding:utf-8 -*-
 __author__ = 'Administrator'
 
-from Page_Model.HomePage import LoginHomePage
+from Web_Page.HomePage import LoginHomePage
 import logging,unittest
-from Page_Model.BaseTestPage import BaseTestCase
+from Web_Page.BaseTestPage import BaseTestCase
 import time as t
-from Page_Model.MessageCount import Message_Count
+from Web_Page.MessageCount import Message_Count
 
 class MyCtoBlog(BaseTestCase, LoginHomePage):
 
-    logging.info(u'logging info: 运行测试用例')
-    def test_001(self, username ='zshzx@126.com',password='xxxxx'):
+    logging.info(u'logging info: 正在运行测试用例...')
+    def test_001(self, username ='@126.com',password=''):
         ''' 测试51cto登录成功后返回消息总数 '''
         self.doLogin(username, password)
         t.sleep(8)
         Message_Count(self.driver).GetMessageCount()
-    logging.info(u'运行测试用例完毕')
+        logging.info(u'testcase_001运行完毕')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
@@ -27,9 +27,3 @@ if __name__ == '__main__':
 # options = webdriver.ChromeOptions()
 # options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
 # driver = webdriver.Chrome(chrome_options=options)
-# 
-# driver.maximize_window()
-# driver.implicitly_wait(15)
-# driver.get('http://www.51cto.com/')
-# t.sleep(5)
-# driver.quit()
